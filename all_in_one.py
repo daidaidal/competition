28aa8d79c60a74a5b41431d3eac4a24b56a3e587
daidaidal <15636811081@163.com>
2018-01-07 20:44:45 +0800

    first train

--- all_in_one.py
+++ all_in_one.py
@@ -83,60 +83,58 @@ def training(input_sentence,trigger_word,trigger_subtype,argument_dic):
     loss_func = torch.nn.CrossEntropyLoss()
 
     input_list = []
-    for t in range(100):
-        for i in range(sentence_len):
-            input_list.clear()
-            input_list.append(h_sum[i].data)
-            input_list.append(getl_trg_i(word_vec_list,i))
-            input_list.append(g_trg)
-            input_cat_vec = torch.cat(input_list,0)
-            input_cat_vec = Variable(input_cat_vec,True)
+    for i in range(sentence_len):
+        input_list.clear()
+        input_list.append(h_sum[i].data)
+        input_list.append(getl_trg_i(word_vec_list, i))
+        input_list.append(g_trg)
+        input_cat_vec = torch.cat(input_list, 0)
+        input_cat_vec = Variable(input_cat_vec, True)
 
-            output1 = f_network(input_cat_vec)   # h2,h_m
-            s = torch.nn.Softmax()
-            index_predic_trigger = np.argmax(s(output1).data.numpy())
+        output1 = f_network(input_cat_vec)  # h2,h_m
+        s = torch.nn.Softmax()
+        index_predic_trigger = np.argmax(s(output1).data.numpy())
 
+        # if len(np.where(output1.data.numpy() == 1)[0])==1:
+        #     index_predic_trigger = np.where(output1.data.numpy() == 1)[0][0]
+        #     g_trg[index_predic_trigger] = 1
 
-            # if len(np.where(output1.data.numpy() == 1)[0])==1:
-            #     index_predic_trigger = np.where(output1.data.numpy() == 1)[0][0]
-            #     g_trg[index_predic_trigger] = 1
+        # if i == trigger_index:
+        #     judge_trigger = True
+        # else:
+        #     judge_trigger = False
+        #
+        # for j in range(entity_num):
+        #     input1 = h_sum[i]  # 600
+        #     input12 = h_sum[entity_dic[j][0]]  # 600
+        #     input2 = getl_trg_i(input_sentence, i)  # 1000
+        #     input3 = getl_trg_i(input_sentence, entity_dic[j][0])  # 1000
+        #     input4 = g_trg_arg[entity_dic[j][0]]  # 33
+        #     input_all = torch.cat(input1, input2, input3, input4)  # 600*2+1000*2+33 = 3233
+        #     output2 = f_network2(input_all)
+        #
+        #     loss2 = loss_func(output2, target_ouput2)  # 计算两者的误差
+        #     optimizer2.zero_grad()  # 清空上一步的残余更新参数值
+        #     loss2.backward()  # 误差反向传播, 计算参数更新值
+        #     optimizer2.step()
+        if i == trigger_index:
+            target_output = Variable(torch.LongTensor([trigger_target_num]))
+        else:
+            target_output = Variable(torch.LongTensor([0]))
+        # loss
+        temp_output = []
+        temp_output.append(output1)
+        output2 = torch.stack(temp_output)
+        loss = loss_func(output2, target_output)  # 计算两者的误差
+        optimizer1.zero_grad()  # 清空上一步的残余更新参数值
+        optimizer_rnn.zero_grad()
+        loss.backward()  # 误差反向传播, 计算参数更新值
 
-            # if i == trigger_index:
-            #     judge_trigger = True
-            # else:
-            #     judge_trigger = False
-            #
-            # for j in range(entity_num):
-            #     input1 = h_sum[i]  # 600
-            #     input12 = h_sum[entity_dic[j][0]]  # 600
-            #     input2 = getl_trg_i(input_sentence, i)  # 1000
-            #     input3 = getl_trg_i(input_sentence, entity_dic[j][0])  # 1000
-            #     input4 = g_trg_arg[entity_dic[j][0]]  # 33
-            #     input_all = torch.cat(input1, input2, input3, input4)  # 600*2+1000*2+33 = 3233
-            #     output2 = f_network2(input_all)
-            #
-            #     loss2 = loss_func(output2, target_ouput2)  # 计算两者的误差
-            #     optimizer2.zero_grad()  # 清空上一步的残余更新参数值
-            #     loss2.backward()  # 误差反向传播, 计算参数更新值
-            #     optimizer2.step()
-            if i == trigger_index:
-                target_output = Variable(torch.LongTensor([trigger_target_num]))
-            else:
-                target_output = Variable(torch.LongTensor([0]))
-            # loss
-            temp_output = []
-            temp_output.append(output1)
-            output2 = torch.stack(temp_output)
-            loss = loss_func(output2,target_output)  # 计算两者的误差
-            optimizer1.zero_grad()  # 清空上一步的残余更新参数值
-            optimizer_rnn.zero_grad()
-            loss.backward()  # 误差反向传播, 计算参数更新值
+        optimizer1.step()
+        optimizer_rnn.step()
 
-            optimizer1.step()
-            optimizer_rnn.step()
-
-    torch.save(f_network, 'net1.pkl')  # 保存整个网络
-    torch.save(rnn, 'rnn.pkl')
+        torch.save(f_network, 'net1.pkl')  # 保存整个网络
+        torch.save(rnn, 'rnn.pkl')
 
 # def testing(input_sentence,trigger_word,trigger_subtype,argument_dic):
 #     #h_sum = gru_vec(input_sentence)  # h_sum[i] to get hi
